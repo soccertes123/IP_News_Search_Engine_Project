e2f3145276422983be2250ae5e4b522c9ca4d790
soccertes123 <tschon@wustl.edu>
2019-05-05 12:43:39 -0500

    adding important scripts

---
+++ createIndex.py
@@ -0,0 +1,76 @@
+import sys
+import re
+import pandas as pd
+import csv
+from string import punctuation
+import pickle
+from porterStemmer import PorterStemmer
+from collections import defaultdict
+from array import array
+import gc
+
+porter=PorterStemmer()
+
+class CreateIndex:
+
+	def __init__(self):
+		self.stopWords_file = "stopwords.dat"
+		self.corpus_file = "testCorpus.csv"
+		self.index_file = "testIndex.pickle"
+		self.index = defaultdict(list) # inverted index
+
+	def storeStopwords(self):
+		# stopwords function influenced by http://www.ardendertat.com/2011/05/30/how-to-implement-a-search-engine-part-1-create-index/
+		with open(self.stopWords_file, 'r') as f:
+			stop_word_list = [line.rstrip('\n') for line in f]
+			self.stopword_list = stop_word_list
+
+	def createIndex(self):
+		# creates an inverse index out of a dictionary to represent word positions in documents
+		corp = pd.read_csv(self.corpus_file, encoding = "mac_roman")
+		for i, body in enumerate(corp['body']):
+			# combine title and body of document
+			txt = corp['title'][i] + " " + body
+			# now get rid of punctuation and case
+			txt = ''.join(c for c in txt if c not in punctuation)
+			txt = txt.lower()
+			wordList = txt.split()
+			
+			# create a temporary dictionary for words in the document
+			tempDict = defaultdict(list)
+			position = 0
+			for word in wordList:
+				w = porter.stem(word, 0, len(word)-1)
+				if w not in self.stopword_list:
+					try:
+						tempDict[w][1].append(position)
+					except:
+						tempDict[w] = [corp['id'][i], [position]]
+					position += 1
+			self.mergeIndex(tempDict)
+		self.saveIndexToFile()
+
+	def mergeIndex(self, localDict):
+		# merges the index with a subindex
+		for word in localDict:
+			self.index[word].append(localDict[word])
+	
+	def saveIndexToFile(self):
+		# saves the index to a pickle file so that it can be loaded later
+		copyDict = self.index
+		pickle_out = open(self.index_file,"wb")
+		pickle.dump(copyDict, pickle_out)
+		pickle_out.close()
+	
+	def loadIndexFromFile(self):
+		# load the pickle file into the index variable
+		pickle_in = open(self.index_file, "rb")
+		self.index = pickle.load(pickle_in)
+
+if __name__=="__main__":
+	c=CreateIndex()
+	c.storeStopwords()
+	c.createIndex()
+	# c.loadIndexFromFile()
+	# for key, value in c.index.items():
+	# 	print(key)
---
+++ query.py
@@ -0,0 +1,96 @@
+import sys
+import re
+import pandas as pd
+import csv
+from string import punctuation
+import pickle
+from porterStemmer import PorterStemmer
+from collections import defaultdict
+from array import array
+import gc
+
+porter=PorterStemmer()
+
+class Query:
+
+	def __init__(self):
+		self.stopWords_file = "stopwords.dat"
+		self.corpus_file = "testCorpus.csv"
+		self.index_file = "testIndex.pickle"
+		self.index=defaultdict(list) # inverted index
+
+	def storeStopwords(self):
+		# stopwords function influenced by http://www.ardendertat.com/2011/05/30/how-to-implement-a-search-engine-part-1-create-index/
+		with open(self.stopWords_file, 'r') as f:
+			stop_word_list = [line.rstrip('\n') for line in f]
+			self.stopword_list = stop_word_list
+	
+	def loadIndexFromFile(self):
+        # load the pickle file into the index variable
+		pickle_in = open(self.index_file,"rb")
+		self.index = pickle.load(pickle_in)
+
+	def getRelevantDocuments(self, query):
+		# performs a phrase query to return relevant documents that have the exact query within the document
+		# will return this list containing relevant docs later
+		relevantDocs = []
+		# whichDocs contains a list of documents for each word where the word appears in the document
+		whichDocs = defaultdict(list)
+		queryList = []
+		for word in query.split():
+			w = porter.stem(word, 0, len(word)-1)
+			if w not in self.stopword_list:
+				queryList.append(w)
+				if w in self.index:
+					whichDocs[w] = []
+					for doc in self.index[w]:
+						# add the document id to the list containing which documents the word appears in
+						whichDocs[w].append(doc[0])
+				else:
+					# query word doesn't exist in index
+					return []
+		if not whichDocs:
+			return None
+		# intersectionList will be the list of documents that contain all query terms
+		intersectionList = list(whichDocs.values())[0]
+		for docList in list(whichDocs.values()):
+			intersectionList = self.intersection(intersectionList, docList)
+		for docId in intersectionList:
+			# for each relevent document, get a dictionary corresponding query words to their positions in the document
+			qWordPositions = [None] * len(queryList)
+			for i, qWord in enumerate(queryList):
+				w = porter.stem(qWord, 0, len(qWord)-1)
+				for documents in self.index[w]:
+					if (documents[0] == docId):
+						# put the list of word positions into the appropriate bucket
+						decrementPositionList = []
+						for position in documents[1]:
+							# subtracting i from the word's position will allow us to compare proximity between words later by intersecting lists
+							decrementPositionList.append(position - i)
+						qWordPositions[i] = decrementPositionList
+			testIntersection = qWordPositions[0]
+			for li in qWordPositions:
+				testIntersection = self.intersection(testIntersection, li)
+			if testIntersection:
+				# document has a match for the query; add it to the relevant documents list
+				relevantDocs.append(docId)
+		return relevantDocs
+
+	def intersection(self, list1, list2):
+		# got this trick from https://www.geeksforgeeks.org/python-intersection-two-lists/
+		return set(list1).intersection(list2)
+
+if __name__=="__main__":
+	q=Query()
+	q.storeStopwords()
+	q.loadIndexFromFile()
+	query = input("Enter a query: ")
+	relevantDocuments = q.getRelevantDocuments(query)
+	if not relevantDocuments:
+		print("Couldn't find any documents to match your query")
+	else:
+		corp = pd.read_csv(q.corpus_file, encoding = "mac_roman")
+		for id in relevantDocuments:
+			# print out relevant titles
+			print(corp['title'][id-1])
+			txt = corp['title'][id-1] + " " + corp['body'][id-1]
